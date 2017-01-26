e867e102fb2cbd34773b233cee95ef0b6322099d
MORGAN-LAPTOP\Morgan <morgan.ryan@my.simpson.edu>
2017-01-26 08:40:59 -0600

    commit

---
+++ src/edu/simpson/cis/cis320/DBHelper.java
@@ -0,0 +1,7 @@
+package edu.simpson.cis.cis320;
+
+/**
+ * Created by Morgan on 1/26/2017.
+ */
+public class DBHelper {
+}
---
+++ src/edu/simpson/cis/cis320/NameListGet.java
@@ -0,0 +1,29 @@
+package edu.simpson.cis.cis320;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import com.google.gson.Gson;
+
+/**
+ * Created by Morgan on 1/26/2017.
+ */
+@WebServlet(name = "NameListGet")
+public class NameListGet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print("{ \"Field\":\"Hello World\"}");
+        //Gson gson = new Gson();
+        //String json = gson.toJson(myObject);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
