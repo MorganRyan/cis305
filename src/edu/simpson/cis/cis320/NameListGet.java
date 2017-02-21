09454a6f19db7ccda363c1c7cbe849081fd97a85
MORGAN-LAPTOP\Morgan <morgan.ryan@my.simpson.edu>
2017-02-20 20:53:01 -0600

    no message

--- src/edu/simpson/cis/cis320/NameListGet.java
+++ src/edu/simpson/cis/cis320/NameListGet.java
@@ -17,7 +17,7 @@ import com.google.gson.Gson;
 public class NameListGet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter(); {
         public void updatePerson(Person person);
         Gson gson = new Gson();
         String json = gson.toJson(updatePerson);
